39711c62bc56274eaf2ca6e35d0c4876823d5abb
Ngawang Sherpa <ngawangt.sherpa777@gmail.com>
2026-08-26 16:29:08 +0545

    Time: 31 ms (41.24%) | Memory: 25.8 MB (92.02%) - LeetSync

--- Arrays/217-contains-duplicate/contains-duplicate.py
+++ Arrays/217-contains-duplicate/contains-duplicate.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        
         seen = set()
-        #we use the property of set .i.e. whether it is present or not ..?
-        #but 1st , we need loop for this comparison and we need the number itself.
-        for number in nums:
-            if number in seen:
+        for num in nums:
+            if num in seen:
                 return True
             else:
-                seen.add(number)
+                seen.add(num)
         return False
