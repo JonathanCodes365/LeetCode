933b7e7085bcf0be862faf0eca1176ff7627ef12
Ngawang Sherpa <ngawangt.sherpa777@gmail.com>
2026-09-07 10:46:40 +0545

    Time: 10 ms (96.66%) | Memory: 12.4 MB (98.72%) - LeetSync

--- Arrays/242-valid-anagram/valid-anagram.py
+++ Arrays/242-valid-anagram/valid-anagram.py
@@ -1,15 +1,6 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        x = len(s)
-        y = len(t)
-
-        if x != y:
-            return False
-        #okay we counted the total number of characters in both of them
-        #if they are not equal they will be naturally returned false and program will terminate.
-
-        #Assuming they have the same length.
-        #Now, 1 way is we can check each and every one of those words 1 by 1.
+        
         count_s={}
         for char in s:
             if char in count_s:
